630ee00b37ea554e3ce05d942fbc8e185f19fee7
Leenima <ggfggfggf123@naver.com>
2021-05-12 09:39:51 +0900

    make MyHandler.js

--- MyHandler.js
+++ MyHandler.js
@@ -1,5 +1,9 @@
 function start(res) {
-    let Body = 'Hello, world! <br> I am in the cloud class.';
+    let Body = '<head><meta charset ="UTF-8"/></head>'
+    body += '<body><div>Hello, world! <br> I am in the cloud class.</div>';
+    body += '<div><a href="\hello">hello 페이지</a></div>' //클릭하면 hello로 가는거
+    body += '<div><a href="\wait">5초 대기 페이지</a></div>' //클릭하면 hello로 가는거
+    body += '</body>'
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(Body);
     res.end();
@@ -12,5 +16,15 @@ function hello(res) {
     res.end();
 }
 
+function wait(res) {
+    setTimeout(function() {
+        let Body = 'This is my first web server.';
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(Body);
+        res.end();
+    }, 5000); // 5초뒤에 실행해라
+}
+
 exports.start = start;
+exports.wait = wait;
 exports.hello = hello;
--- upload.bat
+++ upload.bat
@@ -1,3 +1,4 @@
+git remote add origin https://github.com/Leenima/NodeServer_2021.git
 git add --all
 git commit -m "make MyHandler.js"
  git push -u origin master
