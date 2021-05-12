eecdb6afbb0ee97066a31c5303392b0a4d5964c9
Leenima <ggfggfggf123@naver.com>
2021-05-12 09:43:03 +0900

    wait page / 踰꾧렇 ?쒓굅

--- MyHandler.js
+++ MyHandler.js
@@ -1,26 +1,26 @@
 function start(res) {
-    let Body = '<head><meta charset ="UTF-8"/></head>'
+    let body = '<head><meta charset ="UTF-8"/></head>'
     body += '<body><div>Hello, world! <br> I am in the cloud class.</div>';
     body += '<div><a href="\hello">hello 페이지</a></div>' //클릭하면 hello로 가는거
     body += '<div><a href="\wait">5초 대기 페이지</a></div>' //클릭하면 hello로 가는거
     body += '</body>'
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(Body);
+    res.write(body);
     res.end();
 }
 
 function hello(res) {
-    let Body = 'This is my first web server.';
+    let body = 'This is my first web server.';
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(Body);
+    res.write(body);
     res.end();
 }
 
 function wait(res) {
     setTimeout(function() {
-        let Body = 'This is my first web server.';
+        let body = 'This is my first web server.';
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(Body);
+        res.write(body);
         res.end();
     }, 5000); // 5초뒤에 실행해라
 }
--- upload.bat
+++ upload.bat
@@ -1,4 +1,4 @@
 git remote add origin https://github.com/Leenima/NodeServer_2021.git
 git add --all
-git commit -m "wait page"
+git commit -m "wait page / 버그 제거"
  git push -u origin master
