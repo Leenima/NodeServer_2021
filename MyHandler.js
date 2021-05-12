18b18f8fc9f7f606d74ffac808bad378e133ba66
Leenima <ggfggfggf123@naver.com>
2021-05-12 09:55:44 +0900

    wait page / 踰꾧렇 ?쒓굅

--- MyHandler.js
+++ MyHandler.js
@@ -2,7 +2,8 @@ function start(res) {
     let body = '<head><meta charset ="UTF-8"/></head>'
     body += '<body><div>Hello, world! <br> I am in the cloud class.</div>';
     body += '<div><a href="\hello">hello 페이지</a></div>' //클릭하면 hello로 가는거
-    body += '<div><a href="\wait">5초 대기 페이지</a></div>' //클릭하면 hello로 가는거
+    body += '<div><a href="\wait">5초 대기 페이지</a></div>' //클릭하면 wait로 가는거
+    body += '<div><a href="\randomWait">무작위 대기 페이지</a></div>' //클릭하면 randomWait로 가는거
     body += '</body>'
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(body);
@@ -25,6 +26,16 @@ function wait(res) {
     }, 5000); // 5초뒤에 실행해라
 }
 
+function randomWait(res) {
+    let waitTime = Math.round(Math.random() * 10000.)
+    setTimeout(function() {
+        let body = 'Thank you for waiting for ' + waitTime + ' ms.';
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(body);
+        res.end();
+    }, waitTime); // 5초뒤에 실행해라
+}
 exports.start = start;
 exports.wait = wait;
+exports.randomWait = randomWait;
 exports.hello = hello;
